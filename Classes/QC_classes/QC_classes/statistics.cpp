12be356529c6c2f0349f798523bcf081f69f16e4
Stanisław Bzdęga <stasiu1997x@gmail.com>
2019-06-14 11:29:38 +0200

    Further working on statistics class. No significant result achieved.

--- Classes/QC_classes/QC_classes/statistics.cpp
+++ Classes/QC_classes/QC_classes/statistics.cpp
@@ -20,16 +20,45 @@ double statistics::QBER(protocol * Alice, protocol * person)
 	return static_cast<double>(counter) / Alice->key.size();
 }
 
-vector<double> statistics::simulate_QBER_noise(protocol * Alice, protocol * person, double min_noise, double max_noise, double step_noise)
+void statistics::simulate_QBER_noise(protocol* Alice, protocol* Bob, double min_noise, double max_noise, double step_noise)
 {
 	double noise_level = min_noise;
 	int no_steps = static_cast<int>((max_noise - min_noise) / step_noise);
+	
+	int size = Alice->key_size;
+	bool B92 = Alice->is_B92;
+	double angle = 0;
+	if (B92)
+	
+	vector< vector<double> > QBER_noise;
+	for (int i = 0; i < no_steps; i++) {
+		delete Alice;
+		delete Bob;
 
-	vector<double> QBER;
-	for (int i = 0; i < no_steps; i++){
+		if (B)
 		
+		Alice->load_key();
+		Alice->generate_basis();
+
+		Bob->load_key();
+		Bob->generate_basis();
+
+		quantum_channel connection(Alice);
+		connection.make_noise(noise_level);
+
+		Bob->read_quantum(&connection);
+		Alice->compare(Bob);
+
+		Alice->key_reduction();
+		Bob->key_reduction();
+		
+		vector<double> Y;
+		Y.push_back(this->QBER(Alice, Bob));
+		Y.push_back(noise_level);
+		QBER_noise.push_back(Y);
+		noise_level += no_steps;
 	}
-	return QBER;
+	this->QBER_vs_noise = QBER_noise;
 }
 
 vector<double> statistics::simulate_QBER_angle(protocol * Alice, protocol * person, double min_angle, double max_angle, double step_angle)
--- Classes/QC_classes/QC_classes/statistics.h
+++ Classes/QC_classes/QC_classes/statistics.h
@@ -5,7 +5,7 @@ using namespace std;
 
 class statistics {
 protected:
-	vector<double> QBER_vs_noise;
+	vector< vector<double> > QBER_vs_noise;
 	vector<double> QBER_correction;
 	int initial_key_size;
 	int key_lenght;
@@ -15,7 +15,7 @@ public:
 	statistics(protocol* Alice, protocol* Bob, protocol* Eve);
 	double QBER(protocol* Alice, protocol* person);
 
-	vector<double> simulate_QBER_noise(protocol* Alice, protocol* person, double min_noise, double max_noise, double step_noise);
+	void simulate_QBER_noise(protocol* Alice, protocol* Bob, double min_noise, double max_noise, double step_noise);
 	vector<double> simulate_QBER_angle(protocol* Alice, protocol* person, double min_angle, double max_angle, double step_angle);
 
 };
